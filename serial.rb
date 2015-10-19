e990ad92963790ce69d5a47e2609377f5d882725
Scott <scsilver.umd@gmil.com>
2015-10-18 20:25:38 -0600

    First Commit

---
+++ serial.rb
@@ -0,0 +1,30 @@
+require "serialport"
+require 'pusher'
+
+port_str = "/dev/tty.usbmodemfd131"
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+Pusher.url = "https://60dbe63931b29368c58c:6f0fac727cfb5a675818@api.pusherapp.com/apps/146837"
+
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+while true do
+  message = sp.gets
+  if message
+    message.chomp!
+    begin
+    Pusher.trigger('sensor_channel', 'data_event', {value: message})
+
+    rescue Pusher::Error => e
+      # (Pusher::AuthenticationError, Pusher::HTTPError, or Pusher::Error)
+    end
+    open('data.csv', 'a') { |file|
+      if message.size > 42
+        file.puts message
+      end
+    }
+    puts message
+    sleep 1
+  end
+end
